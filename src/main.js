8f209b390b80c98df54aefa605fc84ffc1ba5852
Vladyslav Popov <vlad_np@ukr.net>
2025-10-09 08:42:58 +0200

    remove all console logs

--- src/main.js
+++ src/main.js
@@ -2,7 +2,5 @@ import { loadMore, onSearch } from './js/render-function';
 import { ref } from './js/CONSTATNT';
 
 // функція пошуку
-console.log('start');
-console.log(ref);
 ref.searchButton.addEventListener('click', onSearch);
 ref.loadMoreButton.addEventListener('click', loadMore);
